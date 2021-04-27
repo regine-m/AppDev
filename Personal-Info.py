37a079763323e74006866a0876a34bf72abf3722
Regine <83236729+regine-m@users.noreply.github.com>
2021-04-27 16:32:02 +0800

    Add files via upload

---
+++ Personal-Info.py
@@ -0,0 +1,6 @@
+fName = input("What is your first name? ")
+lName = input("What is your last name? ")
+loc = input("What is  your location? ")
+age = input("What is your age? ")
+
+print("Hi " + fName + " " + lName + "! Your location is " + loc + " and you are " + age + " years old.")
